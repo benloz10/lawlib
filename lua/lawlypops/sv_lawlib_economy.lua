17e0fd36109409ac32f1e8db3aca28fac61fdd3d
Twiggle <benloz10@yahoo.com>
2023-08-18 14:24:12 -0500

    Started working on economy functionality

---
+++ lua/lawlypops/sv_lawlib_economy.lua
@@ -0,0 +1,15 @@
+util.AddNetworkString("lawlib_economy")
+
+LAWLIB.ShopItems = {}
+
+function LAWLIB:AddShopItem(tbl)
+
+end
+
+function LAWLIB:GetShopItem(itemName)
+
+end
+
+function LAWLIB:GetShopItemNames()
+
+end
