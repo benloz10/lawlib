549e2343fe625402df0cbc51f2c3a1baaf88e89c
Lawlypops <benloz10@yahoo.com>
2023-10-10 11:49:16 -0700

    Added a couple of custom fonts

---
+++ lua/lawlypops/cl_lawlib_fonts.lua
@@ -0,0 +1,10 @@
+surface.CreateFont("LAWLIB:ExtraLarge", {
+    font = "Arial",
+    size = 80,
+    weight = 1000
+})
+surface.CreateFont("LAWLIB:Large", {
+    font = "Arial",
+    size = 40,
+    weight = 1000
+})
