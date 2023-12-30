883f852e5a387017dd99d201d4123fd9e2805005
Lawlypops <benloz10@yahoo.com>
2023-12-29 22:54:39 -0600

    Added LAWLIB:GetVisiblePlayers(ply, dist)

---
+++ lua/lawlypops/sh_lawlib_players.lua
@@ -0,0 +1,27 @@
+function LAWLIB:GetVisiblePlayers(ply, dist)
+    if not IsValid(ply) then return {} end
+    if dist == nil then dist = 0 end
+    local plyList = player.GetAll()
+    local visible = {}
+    --Avoid doing root operations in per-frame hooks.
+    local maxviewDist = dist * dist
+
+    for i=1, #plyList do
+        local target = plyList[i]
+        
+        local distCheck = true
+        if dist > 0 and ply:GetPos():DistToSqr(target:GetPos()) > maxviewDist then
+            distCheck = false
+        end
+
+        local target = plyList[i]
+        if target ~= ply and
+        distCheck and
+        target:IsLineOfSightClear(ply) and
+        target:Health() > 0 and
+        not target:InVehicle() then
+            table.insert(visible, target)
+        end
+    end
+    return visible
+end
