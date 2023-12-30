f1aed52a886f4c2a37810c9fdb0bf6bd49f589a0
Lawlypops <benloz10@yahoo.com>
2023-12-29 22:38:00 -0600

    Added some fonts

--- lua/lawlypops/cl_lawlib_fonts.lua
+++ lua/lawlypops/cl_lawlib_fonts.lua
@@ -1,17 +1,31 @@
-surface.CreateFont("LAWLIB:ExtraLarge", {
+surface.CreateFont("LAWLIB:Medium", {
     font = "Arial",
-    size = 80,
+    size = 28,
     weight = 1000
 })
+
 surface.CreateFont("LAWLIB:Large", {
     font = "Arial",
     size = 40,
     weight = 1000
 })
 
+surface.CreateFont("LAWLIB:ExtraLarge", {
+    font = "Arial",
+    size = 80,
+    weight = 1000
+})
+
 surface.CreateFont("LAWLIB:Monospace", {
     font = "Consolas",
     size = 20
+})
+
+surface.CreateFont("LAWLIB:Monospace:Thicc", {
+    font = "Consolas",
+    size = 20,
+    weight = 1000,
+    bold = true
 })
 
 surface.CreateFont("LAWLIB:Monospace:Medium", {
